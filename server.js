1f465f1f61271560dda856a0748b7193cc2125c7
Asel <asel.peiris@arimaclanka.com>
2022-08-17 23:07:11 +0530

    Initial server.js file setup

---
+++ server.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+const express = require('express');
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.send('Slides Backend running successfully!')
+});
+
+
+app.listen(process.env.PORT, ()=> {
+    console.log(`Server running on port ${process.env.PORT} successfully.`)
+})
